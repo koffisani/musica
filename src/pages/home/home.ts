e56e35f4830fc105f8ba1371dcbafebbf9db3c88
Koffi Sani <koffisani@gmail.com>
2018-04-03 15:16:41 +0000

    Ajout de loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { MusicProvider } from '../../providers/music/music';
 
 @Component({
@@ -11,12 +11,20 @@ export class HomePage {
   public allMusic = [];
 
   constructor(private musicProvider: MusicProvider, 
+    private loadingController: LoadingController,
     public navCtrl: NavController) {
       
   }
   ionViewDidLoad () {
+    let allMusicLoadingController = this.loadingController.create({
+      content: "Getting your music from server"
+    });
+    allMusicLoadingController.present();
     this.musicProvider.getMusic()
-        .subscribe((musicList: any) => this.allMusic = musicList);
+        .subscribe((musicList: any) => {
+          allMusicLoadingController.dismiss();
+          this.allMusic = musicList
+        });
   }
 
 }
