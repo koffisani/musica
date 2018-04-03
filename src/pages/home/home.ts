81431dc3d6cdb770867a17b4671106f8cce46c9c
Koffi Sani <koffisani@gmail.com>
2018-04-03 14:23:33 +0000

    Appel d'une API et utilisation d'un provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MusicProvider } from '../../providers/music/music';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public allMusic = [];
 
+  constructor(private musicProvider: MusicProvider, 
+    public navCtrl: NavController) {
+      
+  }
+  ionViewDidLoad () {
+    this.musicProvider.getMusic()
+        .subscribe((musicList: any) => this.allMusic = musicList);
   }
 
 }
